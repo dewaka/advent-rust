11ec13a985ab6061dba33dd89970ee5398a142e5
Chathura Colombage <dcdewaka@gmail.com>
2018-03-19 21:53:33 +0000

    2015 - problem 2, completed

--- advent15/src/day2.rs
+++ advent15/src/day2.rs
@@ -1,6 +1,6 @@
 // https://adventofcode.com/2015/day/2
-
 use std::cmp;
+use std::io::{self, BufRead};
 
 struct Dimensions {
     width: i32,
@@ -25,19 +25,52 @@ fn required_paper(d: &Dimensions) -> i32 {
     surface_area(d) + slack(d)
 }
 
-fn print_solution(d: &Dimensions) {
-    println!(
-        "{}x{}x{} requires {} square feet of paper",
-        d.width,
-        d.height,
-        d.length,
-        required_paper(d)
-    );
+fn parse_dimension(ds: &String) -> Result<Dimensions, String> {
+    let comps: Vec<&str> = ds.split("x").collect();
+    if comps.len() != 3 {
+        return Err("invalid dimensions - 3 components required".to_owned());
+    }
+
+    match comps[0].parse::<i32>() {
+        Ok(w) => match comps[1].parse::<i32>() {
+            Ok(l) => match comps[2].parse::<i32>() {
+                Ok(h) => Ok(Dimensions {
+                    width: w,
+                    height: h,
+                    length: l,
+                }),
+                Err(_) => Err("parsing height failed".to_owned()),
+            },
+            Err(_) => Err("parsing length failed".to_owned()),
+        },
+        Err(_) => Err("parsing width failed".to_owned()),
+    }
+}
+
+fn compute_aggregate(ds: &Vec<String>) -> i32 {
+    let mut sum = 0;
+
+    for line in ds {
+        parse_dimension(line).map(|d| sum += required_paper(&d));
+    }
+
+    sum
 }
 
 pub fn problem() {
-    println!("2015, day 2");
+    let stdin = io::stdin();
+    let mut ds: Vec<String> = vec![];
 
+    for ln in stdin.lock().lines() {
+        ds.push(ln.unwrap());
+    }
+
+    let agg = compute_aggregate(&ds);
+    println!("Total required: {}", agg);
+}
+
+#[test]
+fn test_examples() {
     let d1 = Dimensions {
         width: 2,
         height: 3,
@@ -50,6 +83,28 @@ pub fn problem() {
         length: 10,
     };
 
-    print_solution(&d1);
-    print_solution(&d2);
+    assert_eq!(58, required_paper(&d1));
+    assert_eq!(43, required_paper(&d2));
+}
+
+#[test]
+fn test_aggregate() {
+    let d1 = Dimensions {
+        width: 2,
+        height: 3,
+        length: 4,
+    };
+
+    let d2 = Dimensions {
+        width: 1,
+        height: 1,
+        length: 10,
+    };
+
+    let expected = required_paper(&d1) + required_paper(&d2);
+
+    assert_eq!(
+        expected,
+        compute_aggregate(&vec!["2x3x4".to_owned(), "1x1x10".to_owned()])
+    )
 }
